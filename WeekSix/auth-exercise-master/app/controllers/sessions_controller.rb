ab05190f9458e481b6d7540dd811b6ba4830044c
Not-A-Sith-Lord <arieldeschapell@gmail.com>
2016-11-22 14:20:53 -0500

    Built user sign in functionality example

---
+++ WeekSix/auth-exercise-master/app/controllers/sessions_controller.rb
@@ -0,0 +1,30 @@
+class SessionsController < ApplicationController
+
+	def new 
+		if session[:user_id]
+			redirect_to "/"
+		else
+		render :new
+		end
+	end
+
+	def create
+		user_maybe = User.find_by(email: params[:email])
+
+		if user_maybe == nil || user_maybe.authenticate(params[:password]) == false
+			redirect_to "/login"
+		else 
+			session[:user_id] = user_maybe.id
+			redirect_to "/"
+		end
+
+	end
+
+	def destroy
+		session.clear
+
+		redirect_to "/"
+	end
+
+	private
+end
